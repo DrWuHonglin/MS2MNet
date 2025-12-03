babbbf6e45ec1885976eb87db1a566b0a77c0577
AIDL <140626676+DrWuHonglin@users.noreply.github.com>
2025-12-03 18:57:50 +0800

    Add files via upload
    上传demo.py

---
+++ demo.py
@@ -0,0 +1,24 @@
+from models.MS2MNet import MS2MNet
+import copy
+import datetime
+import os
+import pandas as pd
+import torchvision
+import torch.nn as nn
+import torch
+from torchvision import models
+import torch.nn.functional as F
+from torchvision.models import ResNet50_Weights
+
+
+if __name__ == "__main__":
+    from thop import profile, clever_format
+
+    x = torch.randn(1, 3, 256, 256)
+    y = torch.randn(1, 1, 256, 256)
+    net = MS2MNet(num_class=6)
+    flops, params = profile(net, inputs=(x, y))
+    macs, params = clever_format([flops, params], "%.3f")
+    print("FLOPs:", macs)
+    print("params:", params)
+
